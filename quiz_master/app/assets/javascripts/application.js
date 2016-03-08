846b91ed70851d589c28a8f856599f037e1278c9
Robert <rob_fong00@yahoo.com>
2016-03-08 15:25:44 +0800

    images linked

--- quiz_master/app/assets/javascripts/application.js
+++ quiz_master/app/assets/javascripts/application.js
@@ -8,10 +8,40 @@
 // compiled file.
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
+// about sinported directives.
 //
 //= require jquery
 //= require jquery_ujs
+//= require j-toker/dist/jquery.j-toker
+//= require jquery-cookie/jquery.cookie
+//= require jquery-deparam/jquery-deparam
 //= require twitter/bootstrap
-//= require turbolinks
 //= require_tree .
+
+$.auth.configure({
+  apiUrl: 'http://localhost:3000'
+})
+
+$('#signup-form').on('submit', function() {
+  $.auth.emailSignUp({
+    email: $('#signup-form input[name="email"]').val(),
+    password: $('#signup-form input[name="password"]').val(),
+    password_confirmation: $('#signup-form input[name=password_confirmation"]').val()
+  }).then(function(user) {
+    alert('Welcome ' + user.name + '!');
+  }).fail(function(resp) {
+    alert('Authentication failure: ' + resp.errors.join(' '));
+  });
+});
+
+$('#signin-form').on('submit', function() {
+  $.auth.emailSignIn({
+    email: $('#signin-form input[name="email"]').val(),
+    password: $('#signin-form input[name="password"]').val(),
+  }).then(function(user) {
+    alert('Welcome back' + user.name + '!');
+  }).fail(function(resp) {
+    alert('Authentication failure: ' + resp.errors.join(' '));
+  });
+});
+
